34a316cd8204df0bc1d41a9d150c0b46881214fa
Miriam <kihitara@gmail.com>
2012-07-03 21:51:02 +1200

    Moving blocks fixed, extra commented out block areas removed from layout

--- layout/general.php
+++ layout/general.php
@@ -3,24 +3,30 @@
 $hasheading = ($PAGE->heading);
 $hasnavbar = (empty($PAGE->layout_options['nonavbar']) && $PAGE->has_navbar());
 $hasfooter = (empty($PAGE->layout_options['nofooter']));
-$hassidepre = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-pre', $OUTPUT));
+// $hassidepre = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-pre', $OUTPUT));
 $hassidepost = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-post', $OUTPUT));
-// $hastopblocks = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('block-top-left', $OUTPUT) or $PAGE->blocks->region_has_content('block-top-mid', $OUTPUT) or  $PAGE->blocks->region_has_content('block-top-right', $OUTPUT));
-// $hasbtmblocks = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('block-btm-left', $OUTPUT) or $PAGE->blocks->region_has_content('block-btm-mid', $OUTPUT) or  $PAGE->blocks->region_has_content('block-btm-right', $OUTPUT));
 
-$showsidepre = ($hassidepre && !$PAGE->blocks->region_completely_docked('side-pre', $OUTPUT));
+// $showsidepre = ($hassidepre && !$PAGE->blocks->region_completely_docked('side-pre', $OUTPUT));
 $showsidepost = ($hassidepost && !$PAGE->blocks->region_completely_docked('side-post', $OUTPUT));
 $custommenu = $OUTPUT->custom_menu();
 $hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));
 
 $bodyclasses = array();
-if ($showsidepre && !$showsidepost) {
-    $bodyclasses[] = 'side-pre-only';
-} else if ($showsidepost && !$showsidepre) {
+if ($showsidepost) {
     $bodyclasses[] = 'side-post-only';
-} else if (!$showsidepost && !$showsidepre) {
+} else if (!$showsidepost) {
     $bodyclasses[] = 'content-only';
 }
+
+// $bodyclasses = array();
+// if ($showsidepre && !$showsidepost) {
+//     $bodyclasses[] = 'side-pre-only';
+// } else if ($showsidepost && !$showsidepre) {
+//     $bodyclasses[] = 'side-post-only';
+// } else if (!$showsidepost && !$showsidepre) {
+//     $bodyclasses[] = 'content-only';
+// }
+
 if ($hascustommenu) {
     $bodyclasses[] = 'has_custom_menu';
 }
@@ -109,36 +115,6 @@ echo $OUTPUT->doctype() ?>
 
     </div>
 
-    <!-- The three blocks added to the top of the page -->
-    <?php // if ($hastopblocks) { ?>
-    <!--
-		    <div id="topblockwrap">
-		    <div class="top-blocks">
-		    <div id="blocktopleft">
-		    <div class="region-content">
-                        <?php // echo $OUTPUT->blocks_for_region('block-top-left') ?>
-                    </div>
-		    </div>
-		    </div>
-		    <div class="top-blocks">
-		    <div id="blocktopmid">
-		    <div class="region-content">
-                        <?php // echo $OUTPUT->blocks_for_region('block-top-mid') ?>
-                    </div>
-		    </div>
-		    </div>
-		    <div class="top-blocks">
-		    <div id="blocktopright">
-		    <div class="region-content">
-                        <?php // echo $OUTPUT->blocks_for_region('block-top-right') ?>
-                    </div>
-		    </div>
-		    </div>
-		    </div>
-    -->
-    <?php // } ?>
-    <!-- End top of page blocks -->
-
 <?php } ?>
 
 <!-- END OF HEADER -->
@@ -179,36 +155,6 @@ echo $OUTPUT->doctype() ?>
         </div>
     </div>
 </div>
-
-    <!-- The three blocks added to the bottom of the page -->
-    <?php // if ($hasbtmblocks) { ?>
-    <!--
-		    <div id="btmblockwrap">
-		    <div class="btm-blocks">
-		    <div id="blockbtmleft">
-		    <div class="region-content">
-                        <?php // echo $OUTPUT->blocks_for_region('block-btm-left') ?>
-                    </div>
-		    </div>
-		    </div>
-		    <div class="btm-blocks">
-		    <div id="blockbtmmid">
-		    <div class="region-content">
-                        <?php // echo $OUTPUT->blocks_for_region('block-btm-mid') ?>
-                    </div>
-		    </div>
-		    </div>
-		    <div class="btm-blocks">
-		    <div id="blockbtmright">
-		    <div class="region-content">
-                        <?php // echo $OUTPUT->blocks_for_region('block-btm-right') ?>
-                    </div>
-		    </div>
-		    </div>
-		    </div>
-    -->
-    <?php // } ?>
-    <!-- End bottom of page blocks -->
 
     </div>
 
